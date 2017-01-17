e6bed8d6fc05d650ff5c30a332115221b8199402
Bernard Ojengwa <bernardojengwa@gmail.com>
2017-01-17 08:24:25 +0100

    add window build bootstrap
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

---
+++ index.js
@@ -0,0 +1,20 @@
+﻿document.write('<b>list of package.json:' + '</b></br>')
+
+const fs = require('fs')
+contents = fs.readFileSync('./package.json', 'utf8')
+
+let p = JSON.parse(contents)
+iter(p)
+
+function iter (p) {
+	for (var key in p) {
+		if (typeof(p[key]) == 'object') {
+			document.write(key + ':</br>');
+	     	 iter(p[key]);
+	    } else {
+	      document.write(key + ' => ' + p[key] + '</br>');
+	    }  
+	    // key is key
+	    // value is p[key]
+	}
+}
